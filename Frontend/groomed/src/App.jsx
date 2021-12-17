9f339a024efb547173bb3523110f0c9835e19e09
Sebastian Maxwell <sebastianrmaxwell@gmail.com>
2021-12-16 21:37:24 -0500

    create openweather widget, waiting for api key to be verified

--- Frontend/groomed/src/App.jsx
+++ Frontend/groomed/src/App.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Calendar from './components/calendar';
+import Weather from './components/weather';
 
 class App extends Component {
     state = { }
     render() {
         return (
-            <><h1>Groomed:</h1><Calendar /></>
+            <>
+            <h1>Groomed:</h1>
+            <Calendar />
+            <Weather />
+            
+            
+            
+            </>
         );
     }
 }
---
+++ Frontend/groomed/src/components/weather.js
@@ -0,0 +1,24 @@
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
+
+const Weather = () => {
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: '1b9b61e3a772c93005fe1304b726a172',
+    lat: '48.137154',
+    lon: '11.576124',
+    lang: 'en',
+    unit: 'metric', // values are (metric, standard, imperial)
+  });
+  return (
+    <ReactWeather
+      isLoading={isLoading}
+      errorMessage={errorMessage}
+      data={data}
+      lang="en"
+      locationLabel="Miami"
+      unitsLabels={{ temperature: 'f', windSpeed: 'mp/h' }}
+      showForecast
+    />
+  );
+};
+
+export default Weather;
